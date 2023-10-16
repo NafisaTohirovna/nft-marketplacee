0651afdc4413cfc1d8d6616e0d35283f16f6bd9c
Nafisa <tohiirovna@gmail.com>
2023-10-16 08:49:38 +0500

    images

--- src/data/data.js
+++ src/data/data.js
@@ -18,19 +18,19 @@ export const navLink = [
 
 export const collections = [
   [
-    "src/assets/collection1.png",
-    "src/assets/collection2.png",
-    "src/assets/collection3.png",
+    "/assets/collection1.png",
+    "/assets/collection2.png",
+    "/assets/collection3.png",
   ],
   [
-    "src/assets/collection4.png",
-    "src/assets/collection5.png",
-    "src/assets/collection6.png",
+    "/assets/collection4.png",
+    "/assets/collection5.png",
+    "/assets/collection6.png",
   ],
   [
-    "src/assets/collection7.png",
-    "src/assets/collection8.png",
-    "src/assets/collection9.png",
+    "/assets/collection7.png",
+    "/assets/collection8.png",
+    "/assets/collection9.png",
   ],
 ];
 
@@ -38,62 +38,62 @@ export const creators = [
   {
     id: 1,
     name: "Keepitreal",
-    avatar: "/src/assets/Avatar Placeholder.png",
+    avatar: "/assets/Avatar Placeholder.png",
   },
   {
     id: 2,
     name: "DigiLab",
-    avatar: "/src/assets/Avatar Placeholder (1).png",
+    avatar: "/assets/Avatar Placeholder (1).png",
   },
   {
     id: 3,
     name: "GravityOne",
-    avatar: "/src/assets/Avatar Placeholder (2).png",
+    avatar: "/assets/Avatar Placeholder (2).png",
   },
   {
     id: 4,
     name: "Juanie",
-    avatar: "/src/assets/Avatar Placeholder (3).png",
+    avatar: "/assets/Avatar Placeholder (3).png",
   },
   {
     id: 5,
     name: "BlueWhale",
-    avatar: "/src/assets/Avatar Placeholder (4).png",
+    avatar: "/assets/Avatar Placeholder (4).png",
   },
   {
     id: 6,
     name: "mr fox",
-    avatar: "/src/assets/Avatar Placeholder (5).png",
+    avatar: "/assets/Avatar Placeholder (5).png",
   },
   {
     id: 7,
     name: "Shroomie",
-    avatar: "/src/assets/Avatar Placeholder (6).png",
+    avatar: "/assets/Avatar Placeholder (6).png",
   },
   {
     id: 8,
     name: "robotica",
-    avatar: "/src/assets/Avatar Placeholder (7).png",
+    avatar: "/assets/Avatar Placeholder (7).png",
   },
   {
     id: 9,
     name: "RustyRobot",
-    avatar: "/src/assets/Avatar Placeholder (8).png",
+    avatar: "/assets/Avatar Placeholder (8).png",
   },
   {
     id: 10,
     name: "animakid",
-    avatar: "/src/assets/Avatar Placeholder (9).png",
+    avatar: "/assets/Avatar Placeholder (9).png",
   },
   {
     id: 11,
     name: "Dotgu",
-    avatar: "/src/assets/Avatar Placeholder (10).png",
+    avatar: "/assets/Avatar Placeholder (10).png",
   },
   {
     id: 12,
     name: "Ghiblier",
-    avatar: "/src/assets/Avatar Placeholder (11).png",
+    avatar: "/assets/Avatar Placeholder (11).png",
   },
 ];
 
@@ -101,50 +101,50 @@ export const categories = [
   {
     id: 1,
     title: "Art",
-    icon: "src/assets/catigoriesIcon1.svg",
-    bg: "src/assets/catigories1.png",
+    icon: "/assets/catigoriesIcon1.svg",
+    bg: "/assets/catigories1.png",
   },
   {
     id: 2,
     title: "Collectibles",
-    icon: "src/assets/catigoriesIcon2.svg",
-    bg: "src/assets/catigories2.png",
+    icon: "/assets/catigoriesIcon2.svg",
+    bg: "/assets/catigories2.png",
   },
   {
     id: 3,
     title: "Music",
-    icon: "src/assets/catigoriesIcon3.svg",
-    bg: "src/assets/catigories3.png",
+    icon: "/assets/catigoriesIcon3.svg",
+    bg: "/assets/catigories3.png",
   },
   {
     id: 4,
     title: "Photography",
-    icon: "src/assets/catigoriesIcon4.svg",
-    bg: "src/assets/catigories4.png",
+    icon: "/assets/catigoriesIcon4.svg",
+    bg: "/assets/catigories4.png",
   },
   {
     id: 5,
     title: "Video",
-    icon: "src/assets/catigoriesIcon5.svg",
-    bg: "src/assets/catigories5.png",
+    icon: "/assets/catigoriesIcon5.svg",
+    bg: "/assets/catigories5.png",
   },
   {
     id: 6,
     title: "Utility",
-    icon: "src/assets/catigoriesIcon6.svg",
-    bg: "src/assets/catigories6.png",
+    icon: "/assets/catigoriesIcon6.svg",
+    bg: "/assets/catigories6.png",
   },
   {
     id: 7,
     title: "Sport",
-    icon: "src/assets/catigoriesIcon7.svg",
-    bg: "src/assets/catigories7.png",
+    icon: "/assets/catigoriesIcon7.svg",
+    bg: "/assets/catigories7.png",
   },
   {
     id: 8,
     title: "Virtual Worlds",
-    icon: "src/assets/catigoriesIcon8.svg",
-    bg: "src/assets/catigories8.png",
+    icon: "/assets/catigoriesIcon8.svg",
+    bg: "/assets/catigories8.png",
   },
 ];
 
@@ -153,43 +153,43 @@ export const discover = [
     id: 1,
     title: "Distant Galaxy",
     name: "MoonDancer",
-    img: "src/assets/discover1.png",
-    avatar: "/src/assets/Avatar Placeholder (1).png",
+    img: "/assets/discover1.png",
+    avatar: "/assets/Avatar Placeholder (1).png",
   },
   {
     id: 2,
     title: "Life On Edena",
     name: "NebulaKid",
-    img: "src/assets/discover2.png",
-    avatar: "/src/assets/Avatar Placeholder (2).png",
+    img: "/assets/discover2.png",
+    avatar: "/assets/Avatar Placeholder (2).png",
   },
   {
     id: 3,
     title: "AstroFiction",
     name: "Spaceone",
-    img: "src/assets/discover3.png",
-    avatar: "/src/assets/Avatar Placeholder (3).png",
+    img: "/assets/discover3.png",
+    avatar: "/assets/Avatar Placeholder (3).png",
   },
 ];
 
 export const works = [
   {
     id: 1,
-    img: "/src/assets/workImg1.png",
+    img: "/assets/workImg1.png",
     title: "Setup Your wallet",
     description:
       "Set up your wallet of choice. Connect it to the Animarket by clicking the wallet icon in the top right corner.",
   },
   {
     id: 2,
-    img: "/src/assets/workImg2.png",
+    img: "/assets/workImg2.png",
     title: "Create Collection",
     description:
       "Upload your work and setup your collection. Add a description, social links and floor price.",
   },
   {
     id: 3,
-    img: "/src/assets/workImg3.png",
+    img: "/assets/workImg3.png",
     title: "Start Earning",
     description:
       "Choose between auctions and fixed-price listings. Start earning by selling your NFTs or trading others.",
